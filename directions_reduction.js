b1d4b66471162c749cf4f756fbe97537857939ac
rifqisambas <rifqisambas@tuta.io>
2020-05-04 20:22:40 +0000

    day 24 - directions reduction

---
+++ directions_reduction.js
@@ -0,0 +1,8 @@
+const dirReduc = (arr) => {
+  let str = arr.join(" ");
+  let re = /NORTH\s+SOUTH|SOUTH\s+NORTH|WEST\s+EAST|EAST\s+WEST/;
+  while (re.test(str)) {
+    str = str.replace(re, "");
+  }
+  return str.split(" ").filter((a) => !!a);
+};
