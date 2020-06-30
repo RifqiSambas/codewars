a43ec5f075eeae3e64f79120c7a5136b5b323a9f
rifqisambas <rifqisambas@tuta.io>
2020-07-01 00:35:17 +0700

    82

--- maximum_subarray_sum.js
+++ maximum_subarray_sum.js
@@ -1,21 +1,12 @@
-let maxSequence = function (arr) {
-  if (arr.filter((a) => a > -1).length < 0 || arr.length <= 0) {
-    return 0;
-  }
-  let maxNum = 0,
-    temp,
-    len = arr.length;
+const maxSequence = (nums) => {
+  let currentSum = 0;
+  let maxSum = 0;
 
-  for (let l = 0, k = 1; l <= len; l++) {
-    temp = arr.slice(l, l + k).reduce((a, b) => a + b, 0);
-    if (temp > maxNum) {
-      maxNum = temp;
-    }
-    if (l === len) {
-      len -= 1;
-      l = -1;
-      k++;
-    }
+  for (let elem of nums) {
+    const nextSum = currentSum + elem;
+    maxSum = Math.max(maxSum, nextSum);
+    currentSum = Math.max(nextSum, 0);
   }
-  return maxNum;
+
+  return maxSum;
 };
