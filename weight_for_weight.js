d12dc29bbbdb74266e66c66f2d888b6a7a83727a
rifqisambas <rifqi@sera5.id>
2020-07-05 05:28:38 +0700

    86

---
+++ weight_for_weight.js
@@ -0,0 +1,7 @@
+const orderWeight = (str) =>
+  str
+    .split(" ")
+    .sort((a, b) => sum(a) - sum(b) || a.localeCompare(b))
+    .join(" ");
+
+const sum = (str) => str.split``.reduce((sum, x) => sum + +x, 0);
